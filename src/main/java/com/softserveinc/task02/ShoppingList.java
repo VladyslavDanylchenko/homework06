438b8d4e33ed4f677c0e4a47042d7c5fd57f65ee
VladyslavDanylchenko <98156931+VladyslavDanylchenko@users.noreply.github.com>
2022-01-24 14:01:00 +0200

    Initial commit

---
+++ src/main/java/com/softserveinc/task02/ShoppingList.java
@@ -0,0 +1,23 @@
+package com.softserveinc.task02;
+
+public class ShoppingList {
+    // TODO: Add the private field items type of ArrayList<String>
+
+    public void add(String product) {
+        // TODO: Add the product to list
+    }
+    
+    public int count() {
+        // TODO: Remove the line below and implement this method. Method should return size of items
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public void delete(String product) {
+        // TODO: delete all products from list
+    }
+
+    public void print() {
+        // TODO: print the list
+    }
+
+}
