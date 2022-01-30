9ef7c2c37f75bcd1ae4759a0f16c6b9143c2c10e
Vladyslav Danylchenko <vladyslavdanylchenko@users.noreply.github.com>
2022-01-30 17:43:20 +0200

    Completed task02 of homework06

--- src/main/java/com/softserveinc/task02/ShoppingList.java
+++ src/main/java/com/softserveinc/task02/ShoppingList.java
@@ -1,23 +1,32 @@
 package com.softserveinc.task02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingList {
     // TODO: Add the private field items type of ArrayList<String>
+    private List<String> items = new ArrayList<>();
 
     public void add(String product) {
         // TODO: Add the product to list
+        items.add(product);
     }
-    
+
     public int count() {
-        // TODO: Remove the line below and implement this method. Method should return size of items
-        throw new RuntimeException("Not implemented yet");
+        // TODO: Method should return size of items
+        return items.size();
     }
 
     public void delete(String product) {
         // TODO: delete all products from list
+        items.remove(product);
     }
 
     public void print() {
         // TODO: print the list
+        for (String item : items) {
+            System.out.println(item);
+        }
     }
 
 }
